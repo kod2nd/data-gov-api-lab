dce4db1180e4133ee4586ac80af3bc2bfc200d14
kod2nd <colinongengkiat@gmail.com>
2018-07-05 20:39:31 +0800

    updated tests

--- app.test.js
+++ app.test.js
@@ -12,10 +12,10 @@ test('should return an array with length greater than 0 ', async () => {
 });
 
 test('/hdb?flat_type=2 should return an array of objects with flat_type 2, denoting 2 room flats', async () => {
-    const response = await request(app).get('/hdb/?flat_type=2');
+    const response = await request(app).get('/hdb/?rooms=2');
     expect(response.status).toEqual(200);
     // expect(response.body.length).toBeGreaterThan(0);
-    expect(response.body[0].flat_type).toContain("2")
+    expect(response.body[0].flat_type[0]).toContain("2")
 });
 
 
@@ -27,6 +27,6 @@ test('query yrstart=2000 should return flats with lease_commence_date of >= 2000
 });
 
 test('for /hdb/?flat_type=NUMBER if number is not 1,2,3,4,5 should return a 404 error with error message', async () => {
-    const response = await request(app).get('/hdb/?flat_type=10');
+    const response = await request(app).get('/hdb/?rooms=10');
     expect(response.status).toEqual(404);
 });
