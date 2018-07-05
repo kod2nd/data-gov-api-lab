21ca0933484740b9ad2d4b779a7e1908a98aa8c7
kod2nd <colinongengkiat@gmail.com>
2018-07-05 13:51:27 +0800

    added swagger UI

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const app = express();
 const hdbRouter = require('./hdb')
+const landingPageRouter = require('./landingPage')
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('./swagger-sample.json')
 
 const errMsg = "Sorry can't find that"
 
-
 app.use(express.json());
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
+// app.use('/', landingPageRouter)
 app.use('/hdb', hdbRouter);
-
 app.use((req, res, next) => {
     res.status(404).json(errMsg);
 })
