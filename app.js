36b7ad7ca3af4c6c6c19f5e6b652f38d6eb66a43
kod2nd <colinongengkiat@gmail.com>
2018-07-05 15:07:13 +0800

    changed swagger api page path to /api-help

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 const express = require("express");
 const app = express();
 const hdbRouter = require('./hdb')
-const landingPageRouter = require('./landingPage')
 const swaggerUi = require('swagger-ui-express')
 const swaggerDocument = require('./swagger-deploy.json')
 
 const errMsg = "Sorry can't find that"
 
+app.use('/api-help', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.use(express.json());
-app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
-// app.use('/', landingPageRouter)
 app.use('/hdb', hdbRouter);
 app.use((req, res, next) => {
     res.status(404).json(errMsg);
